fa1e8e079924dece03d17420083f2923efb3af63
Camilo Andrés Díaz Silva <caadiazsi@unal.edu.co>
2018-02-25 14:36:47 -0500

    Add_Functions

--- Fibonacci/src/main.java
+++ Fibonacci/src/main.java
@@ -5,11 +5,100 @@ import java.math.*;
 import java.util.regex.*;
 
 public class main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void Fibonnacci_Byte() {
 		int N =1;
 		while (N>0) {
+			System.out.println("Ingrese N, 0 para salir.");
+			N = sc.nextInt();
+			byte one =  1;
+			byte two =  2;
+			byte Help =  0;
+			if (N == 2) {
+				Help =  1;
+			} else {
+				for (int i = 1; i < N - 1; i++) {
+					Help = (byte) (one + two);
+					one = two;
+					two = Help;
+				}
+			}
+			System.out.println(Help);
+			System.out.println("---------------------");
+		}
+	}
+	
+	public static void Fibonnacci_Short() {
+		int N =1;
+		while (N>0) {
+			System.out.println("Ingrese N, 0 para salir.");
+			N = sc.nextInt();
+			short one =  1;
+			short two =  2;
+			short Help =  0;
+			if (N == 2) {
+				Help =  1;
+			} else {
+				for (int i = 1; i < N - 1; i++) {
+					Help = (short) (one + two);
+					one = two;
+					two = Help;
+				}
+			}
+			System.out.println(Help);
+			System.out.println("---------------------");
+		}
+	}
+	
+	public static void Fibonnacci_Int() {
+		int N =1;
+		while (N>0) {
+			System.out.println("Ingrese N, 0 para salir.");
+			N = sc.nextInt();
+			int one =  1;
+			int two =  2;
+			int Help =  0;
+			if (N == 2) {
+				Help =  1;
+			} else {
+				for (int i = 1; i < N - 1; i++) {
+					Help = one + two;
+					one = two;
+					two = Help;
+				}
+			}
+			System.out.println(Help);
+			System.out.println("---------------------");
+		}
+	}
+	
+	public static void Fibonnacci_Long() {
+		int N =1;
+		while (N>0) {
+			System.out.println("Ingrese N, 0 para salir.");
+			N = sc.nextInt();
+			Long one = (long) 1;
+			Long two = (long) 2;
+			Long Help = (long) 0;
+			if (N == 2) {
+				Help = (long) 1;
+			} else {
+				for (int i = 1; i < N - 1; i++) {
+					Help = one + two;
+					one = two;
+					two = Help;
+				}
+			}
+			System.out.println(Help);
+			System.out.println("---------------------");
+		}
+	}
+	
+	public static void Fibonnacci_BigInteger() {
+		int N =1;
+		while (N>0) {
+			System.out.println("Ingrese N, 0 para salir.");
 			N = sc.nextInt();
 			BigInteger one = BigInteger.ZERO;
 			BigInteger two = BigInteger.ONE;
