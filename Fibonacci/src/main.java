b5550d9ec3be30cc03aee51324fbc572867feb8f
Camilo Andrés Díaz Silva <caadiazsi@unal.edu.co>
2018-02-25 14:49:07 -0500

    Interactive_Menu
    * Orthography Corrections
    * Menu

--- Fibonacci/src/main.java
+++ Fibonacci/src/main.java
@@ -7,7 +7,7 @@ import java.util.regex.*;
 public class main {
 	static Scanner sc = new Scanner(System.in);
 	
-	public static void Fibonnacci_Byte() {
+	public static void Fibonacci_Byte() {
 		int N =1;
 		while (N>0) {
 			System.out.println("Ingrese N, 0 para salir.");
@@ -29,7 +29,7 @@ public class main {
 		}
 	}
 	
-	public static void Fibonnacci_Short() {
+	public static void Fibonacci_Short() {
 		int N =1;
 		while (N>0) {
 			System.out.println("Ingrese N, 0 para salir.");
@@ -51,7 +51,7 @@ public class main {
 		}
 	}
 	
-	public static void Fibonnacci_Int() {
+	public static void Fibonacci_Int() {
 		int N =1;
 		while (N>0) {
 			System.out.println("Ingrese N, 0 para salir.");
@@ -73,7 +73,7 @@ public class main {
 		}
 	}
 	
-	public static void Fibonnacci_Long() {
+	public static void Fibonacci_Long() {
 		int N =1;
 		while (N>0) {
 			System.out.println("Ingrese N, 0 para salir.");
@@ -95,7 +95,7 @@ public class main {
 		}
 	}
 	
-	public static void Fibonnacci_BigInteger() {
+	public static void Fibonacci_BigInteger() {
 		int N =1;
 		while (N>0) {
 			System.out.println("Ingrese N, 0 para salir.");
@@ -116,9 +116,45 @@ public class main {
 			System.out.println("---------------------");
 		}
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Fibonacci");
+		System.out.println("Que tipo de dato desea utilizar");
+		System.out.println("1. Byte");
+		System.out.println("2. Short");
+		System.out.println("3. Int");
+		System.out.println("4. Long");
+		System.out.println("5. Big Integer");
+		System.out.println("6. End");
+		int Option;
+		Option = sc.nextInt();
+		if(Option==1) {
+			System.out.println("-------Byte-------");
+			Fibonacci_Byte();
+			main(args);
+		}else if(Option==2) {
+			System.out.println("------Short-------");
+			Fibonacci_Short();
+			main(args);
+		}else if(Option==3) {
+			System.out.println("-------Int--------");
+			Fibonacci_Int();
+			main(args);
+		}else if(Option==4) {
+			System.out.println("-------Long-------");
+			Fibonacci_Long();
+			main(args);
+		}else if(Option==5) {
+			System.out.println("----BigInteger----");
+			Fibonacci_BigInteger();
+			main(args);
+		}else{
+			System.out.println("Later");
+		}
+	}
 }
-//byte 12
-//short 110
-//int 1700
-//long 3422
-//BigInteger 22396
+//byte 11
+//short 109
+//int 1699
+//long 3421
+//BigInteger 22397
